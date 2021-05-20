27cac26c17dc735a6d8a13ba22ab77e111245115
Jayesh Jain <jayeshjain1152@gmail.com>
2021-05-20 23:32:34 +0530

    updated logic to show updates of last 15 secs all together

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -2,6 +2,9 @@ from flask import Blueprint, json, request
 from app.extensions import mongo
 import flask_pymongo as pymongo
 from datetime import datetime
+from multiprocessing import Value
+
+counter = Value('i', 0)
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
@@ -10,7 +13,9 @@ def receiver():
 
     data = request.json
     actions_data = mongo.db.github_actions
-    actions_data.insert({"request_id": data['commits'][0]['id'], "author": data['head_commit']['author']['username'], "action": "PUSH", "from_branch": data['ref'].split("/")[-1], "to_branch": data['ref'].split("/")[-1], "timestamp": data['head_commit']['timestamp']})
+    actions_data.insert({"request_id": data['commits'][0]['id'], "author": data['head_commit']['author']['username'], "action": "PUSH", "from_branch": data['ref'].split("/")[-1], "to_branch": data['ref'].split("/")[-1], "timestamp": data['repository']['updated_at']})
+    with counter.get_lock():
+        counter.value += 1
     return json.dumps({"Action" : "request accepted and stored in collections"}), 200
 
 @webhook.route('/pullrequest', methods=["POST"])
@@ -22,14 +27,20 @@ def pullrequest():
         actions_data.insert({"request_id": str(data['pull_request']['id']), "author": data['sender']['login'], "action": "MERGE", "from_branch": data['pull_request']['base']['ref'], "to_branch": data['pull_request']['head']['ref'], "timestamp": data['pull_request']['merged_at']})
     else:
         actions_data.insert({"request_id": str(data['pull_request']['id']), "author": data['sender']['login'], "action": "PULL_REQUEST", "from_branch": data['pull_request']['base']['ref'], "to_branch": data['pull_request']['head']['ref'], "timestamp": data['pull_request']['updated_at']})
-
+    with counter.get_lock():
+        counter.value += 1
     return json.dumps({"Action" : "request accepted and stored in collections"}), 200
 
 @webhook.route('', methods=["GET"])
 def action_update():
     actions_data = mongo.db.github_actions
-    data = actions_data.find().sort("_id",-1).limit(1)
-    statement = "Nothing in database collection"
+    with counter.get_lock():
+        count = counter.value
+        counter.value = 0
+    count = 1 if count == 0 else count
+    data = actions_data.find().sort("_id",-1).limit(count)
+    # self.count = 0
+    line = list()
     for i in data:
         time = datetime.strptime(i['timestamp'], "%Y-%m-%dT%H:%M:%S%z").strftime("%d %b %-I:%M %p %Z")
         if i['action'] == 'PUSH':
@@ -38,7 +49,8 @@ def action_update():
             statement = i['author'] + " submitted a pull request from " + i['from_branch'] + " to " + i['to_branch'] + " on " + time
         elif i['action'] == 'MERGE':
             statement = i['author'] + " merged branch " + i['from_branch'] + " to " + i['to_branch'] + " on " + time
-    
+        line.append(statement)
+    formattedOutput = "<br>".join(line)
     # statement = statement if statement else "Nothing in database collection"
     return """<meta http-equiv="refresh" content="15" /> 
-            Last Update!<br><br>{}""".format(statement)
+            Last Update!<br><br>{}""".format(formattedOutput)
